455c5f8ec81b01e88549df96a1acf47ed56a5f78
Matthew Treinish <mtreinish@kortar.org>
2018-05-17 11:26:40 -0400

    Fix some small issues
    This commit fixes some small issues with various parts of the
    deployment. This is mostly just missing dependencies in the images'
    Dockerfiles and a couple of syntax errors. The real change of note
    though is the initContainer used for DB migrations is switched to have
    the command in the k8s yaml instead of the image build.

--- images/database/run_db_migrations.sh
+++ images/database/run_db_migrations.sh
@@ -1,3 +1,13 @@
-DB_URI="postgresql+psycopg2://$DB_USERNAME:$DB_PASSWORD@$DB_HOST/$DB_NAME"
-
-subunit2sql-db-manage --database-connection $DB_URI upgrade head
+#!/bin/bash
+set -x
+for i in {1..300}; do
+    DB_HOST=$(kubectl get svc ${POSTGRES_CONTAINER_NAME} -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+    if [[ -z "$MQTT_HOST" ]]; then
+        sleep 2
+    else
+        kubectl create secret generic ${DB_SECRET_NAME} --from-literal=host=${DB_HOST}
+        DB_URI="postgresql+psycopg2://$DB_USERNAME:$DB_PASSWORD@$DB_HOST/$DB_NAME"
+        subunit2sql-db-manage --database-connection $DB_URI upgrade head
+        exit 0
+    fi
+exit 1
