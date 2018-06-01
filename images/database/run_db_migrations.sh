6437c38659056c38cd42e94b2ba5b63ee9b47c69
Andrea Frittoli <andrea.frittoli@gmail.com>
2018-06-01 16:07:56 +0100

    Use the pgsql service name
    Use the pqssql service name when running DB migrations and to
    configure the API service as well.

--- images/database/run_db_migrations.sh
+++ images/database/run_db_migrations.sh
@@ -2,16 +2,5 @@
 
 set -x
 
-for i in {1..300}; do
-    DB_HOST=$(/usr/bin/kubectl get svc ${POSTGRES_CONTAINER_NAME} -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-    if [[ -z "$DB_HOST" ]]; then
-        sleep 2
-    else
-        /usr/bin/kubectl create secret generic ${DB_SECRET_NAME} --from-literal=host=${DB_HOST}
-        DB_URI="postgresql+psycopg2://$DB_USERNAME:$DB_PASSWORD@$DB_HOST/$DB_NAME"
-        /usr/local/bin/subunit2sql-db-manage --database-connection $DB_URI upgrade head
-        exit 0
-    fi
-done
-
-exit 1
+DB_URI="postgresql+psycopg2://$DB_USERNAME:$DB_PASSWORD@$DB_HOST/$DB_NAME"
+/usr/local/bin/subunit2sql-db-manage --database-connection $DB_URI upgrade head
